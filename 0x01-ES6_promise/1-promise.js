805c9951a2ef2bf74f1990fa63e2ce82bbbc6d1e
Yofty <106748943+Yofty@users.noreply.github.com>
2023-04-19 09:05:34 -0700

    task 0 - 2

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,9 @@
+export default function getResponseFromAPI(success) {
+  return new Promise((resoleve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
