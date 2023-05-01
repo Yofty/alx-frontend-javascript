5eb7a42e00984f2cd61d66d3a6f16dfd8864d997
Yofty <106748943+Yofty@users.noreply.github.com>
2023-05-01 09:46:22 -0700

    task2

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,59 @@
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  getToWork(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Cannot work from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Cannot have a break';
+  }
+
+  workTeacherTasks(): string  {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: number | string): Teacher | Director {
+    return typeof salary === 'number' && salary < 500 ? new Teacher() : new Director();
+}
+
+const isDirector = (employee: Teacher | Director): boolean =>  employee instanceof Director;
+
+const  executeWork = (employee: Teacher | Director): string =>{
+  let stat;
+  isDirector(employee) ? stat = (employee as Director).workDirectorTasks() : stat = (employee as Teacher).workTeacherTasks();
+  return stat;
+}
+
+type Subjects = 'Math' | 'History';
+
+const teachClass = (todayClass: Subjects): string => todayClass == 'Math' ? 'Teaching Math' : 'Teaching History';
