1e58814ca1d700224cb3eef8122ccaf240e781bc
Yofty <106748943+Yofty@users.noreply.github.com>
2023-05-27 07:56:00 -0700

    task 13 correction

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,10 +1,12 @@
 export default function createIteratorObject(report) {
-  const workers =[];
-  for (const [department, employees] of Object.entries(report.allEmployees)) {
-    for ( const employe of employees) {
-      workers.push(employe);
+  const emps = [];
+  // eslint-disable-next-line
+  for (const dep of Object.keys(report.allEmployees)) {
+    // eslint-disable-next-line
+    for (const emp of report.allEmployees[dep]) {
+      emps.push(emp);
     }
   }
 
-  return workers;
+  return emps;
 }
