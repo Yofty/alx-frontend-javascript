537b308a4febb17dec7eae047cc7e5537ade5646
Yofty <106748943+Yofty@users.noreply.github.com>
2023-05-27 02:39:29 -0700

    correction task 2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,11 @@
-export default function (trueOrFalse) {
+export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
   if (trueOrFalse) {
+    // eslint-disable-nex-line
     const task = true;
+    // eslint-disable-next-line
     const task2 = false;
   }
 
