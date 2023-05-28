f1cc693cf5f356ae7d6026696cfcc1639aa69424
Yofty <106748943+Yofty@users.noreply.github.com>
2023-05-28 09:28:44 -0700

    task 5 correction2

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,10 +1,10 @@
 export default class Building {
   constructor(sqft = 0) {
     if (this.constructor !== Building
-        && typeof this.evacuationWarningMessage !== 'function'
-    ) {
-      throw new Error('Class extending Buidig must override evacuationWarningMessage');
+        && typeof this.evacuationWarningMessage !== 'function') {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+
     this._sqft = sqft;
   }
 
