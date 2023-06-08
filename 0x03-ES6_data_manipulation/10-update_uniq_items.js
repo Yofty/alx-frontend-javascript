9f65ae8bf3d646138135fa09985224a97d6bedbc
root <106748943+Yofty@users.noreply.github.com>
2023-06-08 06:28:50 -0700

    3- 10

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,12 @@
+export default function updateUniqueItem(map) {
+  if (map instanceof Map) {
+    for (const [key, value] of map) {
+      if (value ===1) {
+        map.set(key, 100);
+      }
+    }
+  } else {
+    throw new Error('Cannot process');
+  }
+  return map;
+}
