803a4f2f40438138acf1c2d39a01ec728a0c557e
root <106748943+Yofty@users.noreply.github.com>
2023-06-08 06:36:54 -0700

    correction 8-10

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,7 @@
-export default function updateUniqueItem(map) {
+export default function updateUniqueItems(map) {
   if (map instanceof Map) {
     for (const [key, value] of map) {
-      if (value ===1) {
+      if (value === 1) {
         map.set(key, 100);
       }
     }
